14a8db8c6160a20d3f3610a5d869efee2ed2872e
Tony <SjasFaceMD@gmail.com>
2021-06-19 02:02:51 +0300

    reworking the whole project structure

--- data_sink.py
+++ data_sink.py
@@ -5,25 +5,41 @@ from datetime import datetime
 import yfinance as yf
 
 
-def get_history_data(symbol: str, start_date: datetime,
-                     end_date: datetime, source: str = "yahoo"):
-    data = data_reader.DataReader(symbol,
-                                  start=start_date,
-                                  end=end_date,
-                                  data_source=source)
-    return data
+class abstract_eye():
+
+    def __init__(self, source: str, symbol:str):
+        self.source = source,
+        self.symbol = symbol
+
+    def get_history_data(self, start_date: datetime,
+                         end_date: datetime, source: str = "yahoo"):
+        data = data_reader.DataReader(symbol,
+                                      start=start_date,
+                                      end=end_date,
+                                      data_source=source)
+        return data
+
+    def save_data(self, data: DataFrame, filename: str):
+        data.to_csv(f'data/{filename}')
+
+    def load_from_csv(self, filename: str, index: str = 'Date'):
+        df = pd.read_csv(f'data/{filename}', header=0,
+                        index_col=index, parse_dates=True)
+        return df
 
 
-def get_live_data_yahoo(symbol: str, period: str = "1d", interval: str = "5m"):
-    data = yf.download(tickers=symbol, period=period, interval=interval)
-    return data
+class YF_eye(abstract_eye):
+    def __init__(self, symbol: str):
+        super.__init__(self, "yahoo", symbol)
+        self.ticks = []
 
+    def get_live_tick(self, symbol: str, period: str = "20m", interval: str = "1m"):
+        data = yf.download(self.symbol, period=period, interval=interval)
+        current_value = data["Adj Close"].iloc[-1]
+        if self.ticks != [] or current_value != self.ticks[-1]:
+            self.ticks.append(data)
+            print("tick added")
+        return data
 
-def save_data(data: DataFrame, filename: str):
-    data.to_csv(f'data/{filename}')
-
-
-def load_from_csv(filename: str, index: str = 'Date'):
-    df = pd.read_csv(f'data/{filename}', header=0,
-                     index_col=index, parse_dates=True)
-    return df
+    def get_ticks(self):
+        return self.ticks
