9107979c1b430722c9227757fcfa0221f08e0e5d
Tony <SjasFaceMD@gmail.com>
2021-08-06 18:21:58 +0300

    sma, rsi and ewm plotting implimented

--- data_ops.py
+++ data_ops.py
@@ -3,16 +3,8 @@ import numpy as np
 import pandas as pd
 
 # Data Source
-import yfinance as yf
+# import yfinance as yf
 from binance.client import Client
-
-
-class yfinance_api():
-
-    def download_symbol():
-        # Get Bitcoin data
-        data = yf.download(tickers='BTC-USD', period='2h', interval='1m')
-        return data
 
 
 class binance_api():
@@ -20,17 +12,73 @@ class binance_api():
         self.client = Client(api_key, api_secret)
 
     def get_current_data(self, symbol):
-        return self.client.get_recent_trades(symbol)
+        raw_data = self.client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE, limit=60)
+        data = [{"open": record[1],
+                 "high": record[2],
+                 "low": record[3],
+                 "close": record[4],
+                 "volume": record[5],
+                 "trades": record[7],
+                 "taker_buy_base": record[8],
+                 "taker_buy_quote": record[9]} for record in raw_data]
+        dataframe = pd.DataFrame(data)
+        return dataframe
+
+    def get_last_day(self, symbol):
+        raw_data = self.client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1HOUR, limit=24)
+        data = [{"open": record[1],
+                 "high": record[2],
+                 "low": record[3],
+                 "close": record[4],
+                 "volume": record[5],
+                 "trades": record[7],
+                 "taker_buy_base": record[8],
+                 "taker_buy_quote": record[9]} for record in raw_data]
+        dataframe = pd.DataFrame(data)
+        return dataframe
+
+    def get_two_week_history(self, symbol):
+        raw_data = self.client.get_historical_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1DAY, limit=14, start_str='2 weeks ago UTC')
+        data = [{"open": record[1],
+                 "high": record[2],
+                 "low": record[3],
+                 "close": record[4],
+                 "volume": record[5],
+                 "trades": record[7],
+                 "taker_buy_base": record[8],
+                 "taker_buy_quote": record[9]} for record in raw_data]
+        dataframe = pd.DataFrame(data)
+        return dataframe
 
 
-def get_sma(data):
-    short_rolling = data.rolling(window=20).mean()
-    long_rolling = data.rolling(window=40).mean()
-    short_rolling.fillna(0, inplace=True), long_rolling.fillna(0, inplace=True)
-    print(long_rolling)
-    return short_rolling, long_rolling
+class technical_indicators():
 
+    def get_sma(data):
+        short_rolling = data["close"].rolling(window=10).mean()
+        long_rolling = data["close"].rolling(window=20).mean()
+        return short_rolling, long_rolling
 
-def get_ema(data):
-    ema_short = data.ewm(span=15, adjust=False).mean()
-    return ema_short
+    def get_ema(data):
+        ema_short = data["close"].ewm(span=15, adjust=False).mean()
+        return ema_short
+
+    def get_rsi(data, periods=14, ema=True):
+        """
+        Returns a pd.Series with the relative strength index.
+        """
+        # close_delta = data["close"].diff()
+
+        # Make two series: one for lower closes and one for higher closes
+        up = data["high"]
+        down = data["low"]
+        if ema:
+            # Use exponential moving average
+            ma_up = up.ewm(com=periods - 1, adjust=True, min_periods=periods).mean()
+            ma_down = down.ewm(com=periods - 1, adjust=True, min_periods=periods).mean()
+        else:
+            # Use simple moving average
+            ma_up = up.rolling(window=periods, adjust=False).mean()
+            ma_down = down.rolling(window=periods, adjust=False).mean()  
+        rsi = ma_up / ma_down
+        rsi = 100 - (100/(1 + rsi))
+        return rsi
--- plotter.py
+++ plotter.py
@@ -1,21 +1,23 @@
 #Data viz
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 
 
 class plotter_engine():
 
     def __init__(self):
         #declare figure
-        self.plot = go.Figure()
+        # self.plot = go.Figure()
+        self.plot = make_subplots(rows=1, cols=2)
 
     def plot_symbol(self, data):
 
         #Candlestick
         self.plot.add_trace(go.Candlestick(x=data.index,
-                        open=data['Open'],
-                        high=data['High'],
-                        low=data['Low'],
-                        close=data['Close'], name='market data'))
+                        open=data['open'],
+                        high=data['high'],
+                        low=data['low'],
+                        close=data['close'], name='market data'), row=1, col=1)
 
         # Add titles
         self.plot.update_layout(
@@ -36,13 +38,15 @@ class plotter_engine():
             )
         )
 
-        #Show
-        # rendered_plot = self.plot.to_image(format="png")
-        # return rendered_plot
-
     def plot_sma(self, short_rolling, long_rolling, data):
-        self.plot.add_trace(go.Scatter(x=short_rolling.index, y=short_rolling, mode='lines', name='short rolling'))
-        self.plot.add_trace(go.Scatter(x=long_rolling.index, y=long_rolling, mode='lines', name='long rolling'))
+        self.plot.add_trace(go.Scatter(x=data.index, y=short_rolling, mode='lines', name='short sma'), row=1, col=1)
+        self.plot.add_trace(go.Scatter(x=data.index, y=long_rolling, mode='lines', name='long sma'), row=1, col=1)
+
+    def plot_ema(self, ema, data):
+        self.plot.add_trace(go.Scatter(x=data.index, y=ema, mode='lines', name='ema'), row=1, col=1)
+
+    def plot_rsi(self, rsi, data):
+        self.plot.add_trace(go.Scatter(x=data.index, y=rsi, mode='lines', name='rsi'), row=1, col=2)
 
     def show_plot(self):
         self.plot.show()
